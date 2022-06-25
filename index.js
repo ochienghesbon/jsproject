54e9a239c48d4b99a6bf514e0d0156e41cb3a45e
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-25 15:37:15 +0300

    updates db.json

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ subscribe.addEventListener('click',async(e)=>{
   // stores the value of the email
   if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(comment))){return alert('invalid email')}
   // regular expression to whether email is in correct format
-  const response=await postData("http://localhost:3000/comments",{
+  const response=await postData("https://jsproject-server.herokuapp.com/comments",{
     // it sends subscribed email to server 
     // await works same way as .then
     comment
   })
-  // the code email.value="" makes the input field empty after email submission
+  // the code email.value="" makes the input field empty after email 
   email.value=""
-  const data=await getData("http://localhost:3000/comments")
+  const data=await getData("https://jsproject-server.herokuapp.com/comments")
   console.log(data)
   alert("email sent")
 })
 async function refresh(){
-  let response=await postData("http://localhost:3000/likes",{})
+  let response=await postData("https://jsproject-server.herokuapp.com/likes",{})
   // console.log(response)
   update.textContent=response.id
 }
@@ -28,7 +28,7 @@ refresh()
 const like=document.getElementById('like-button')
     like.addEventListener('click',async(e)=>{
         e.preventDefault()
-        let response=await postData("http://localhost:3000/likes",{})
+        let response=await postData("https://jsproject-server.herokuapp.com/likes",{})
         update.textContent=response.id
   console.log(response)
 like.style.backgroundColor=response.id%2==0?"green":"red"
@@ -82,7 +82,7 @@ like.style.backgroundColor=response.id%2==0?"green":"red"
       const Delete=document.getElementById("deleteemail")
       Delete.addEventListener('click',()=>{
         const idinput=document.getElementById("emailid")
-        deleteEmail("http://localhost:3000/comments/"+idinput.value)
+        deleteEmail("https://jsproject-server.herokuapp.com/comments"+idinput.value)
         alert('deleted')
 
       })
